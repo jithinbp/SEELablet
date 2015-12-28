a30d8ece8691db0a735704ff3e9b7de3bc506d5a
georgesk <georgesk@debian.org>
2015-12-28 13:51:29 +0100

    changed numeric values to byte strings

--- SEEL/commands_proto.py
+++ SEEL/commands_proto.py
@@ -1,226 +1,233 @@
-import math,sys,time
+from __future__ import print_function
 
-ACKNOWLEDGE = 254
+import math,sys,time, struct
+
+# allows to pack numeric values into byte strings
+Byte =     struct.Struct("B") # size 1
+ShortInt = struct.Struct("H") # size 2
+Integer=   struct.Struct("I") # size 4
+
+ACKNOWLEDGE = Byte.pack(254)
 
 #/*----flash memory----*/
-FLASH =1
-READ_FLASH  = 1
-WRITE_FLASH = 2
-WRITE_BULK_FLASH = 3
-READ_BULK_FLASH = 4
+FLASH =Byte.pack(1)
+READ_FLASH       = Byte.pack(1)
+WRITE_FLASH      = Byte.pack(2)
+WRITE_BULK_FLASH = Byte.pack(3)
+READ_BULK_FLASH  = Byte.pack(4)
 
 #/*-----ADC------*/
-ADC =2
-CAPTURE_ONE= 1
-CAPTURE_TWO= 2
-CAPTURE_DMASPEED =3
-CAPTURE_FOUR =4
-CONFIGURE_TRIGGER =5
-GET_CAPTURE_STATUS= 6
-GET_CAPTURE_CHANNEL= 7
-SET_PGA_GAIN= 8
-GET_VOLTAGE =9
-GET_VOLTAGE_SUMMED= 10
-START_ADC_STREAMING =11
-SELECT_PGA_CHANNEL=12
-CAPTURE_12BIT= 13
-CAPTURE_MULTIPLE =14
-SET_HI_CAPTURE = 15
-SET_LO_CAPTURE = 16
+ADC                 = Byte.pack(2)
+CAPTURE_ONE         = Byte.pack(1)
+CAPTURE_TWO         = Byte.pack(2)
+CAPTURE_DMASPEED    = Byte.pack(3)
+CAPTURE_FOUR        = Byte.pack(4)
+CONFIGURE_TRIGGER   = Byte.pack(5)
+GET_CAPTURE_STATUS  = Byte.pack(6)
+GET_CAPTURE_CHANNEL = Byte.pack(7)
+SET_PGA_GAIN        = Byte.pack(8)
+GET_VOLTAGE         = Byte.pack(9)
+GET_VOLTAGE_SUMMED  = Byte.pack(10)
+START_ADC_STREAMING = Byte.pack(11)
+SELECT_PGA_CHANNEL  = Byte.pack(12)
+CAPTURE_12BIT       = Byte.pack(13)
 
 #/*-----SPI--------*/
-SPI_HEADER =3
-START_SPI =1
-SEND_SPI8= 2
-SEND_SPI16= 3
-STOP_SPI =4
-SET_SPI_PARAMETERS =5
-SEND_SPI8_BURST= 6
-SEND_SPI16_BURST= 7
+SPI_HEADER         = Byte.pack(3)
+START_SPI          = Byte.pack(1)
+SEND_SPI8          = Byte.pack(2)
+SEND_SPI16         = Byte.pack(3)
+STOP_SPI           = Byte.pack(4)
+SET_SPI_PARAMETERS = Byte.pack(5)
+SEND_SPI8_BURST    = Byte.pack(6)
+SEND_SPI16_BURST   = Byte.pack(7)
 #/*------I2C-------*/
-I2C_HEADER= 4
-I2C_START =1
-I2C_SEND =2
-I2C_STOP =3
-I2C_RESTART =4
-I2C_READ_END =5
-I2C_READ_MORE =6
-I2C_WAIT =7
-I2C_SEND_BURST =8
-I2C_CONFIG = 9
-I2C_STATUS = 10
-I2C_READ_BULK = 11
-I2C_WRITE_BULK = 12
-I2C_ENABLE_SMBUS = 13
-I2C_INIT = 14
-I2C_PULLDOWN_SCL = 15
+I2C_HEADER       = Byte.pack(4)
+I2C_START        = Byte.pack(1)
+I2C_SEND         = Byte.pack(2)
+I2C_STOP         = Byte.pack(3)
+I2C_RESTART      = Byte.pack(4)
+I2C_READ_END     = Byte.pack(5)
+I2C_READ_MORE    = Byte.pack(6)
+I2C_WAIT         = Byte.pack(7)
+I2C_SEND_BURST   = Byte.pack(8)
+I2C_CONFIG       = Byte.pack(9)
+I2C_STATUS       = Byte.pack(10)
+I2C_READ_BULK    = Byte.pack(11)
+I2C_WRITE_BULK   = Byte.pack(12)
+I2C_ENABLE_SMBUS = Byte.pack(13)
+I2C_INIT         = Byte.pack(14)
+I2C_PULLDOWN_SCL = Byte.pack(15)
 #/*------UART2--------*/
-UART_2= 5
-SEND_CHAR =1
-SEND_INT= 2
-SEND_ADDRESS= 3
-SET_BAUD= 4
-SET_MODE= 5
+UART_2       = Byte.pack(5)
+SEND_CHAR    = Byte.pack(1)
+SEND_INT     = Byte.pack(2)
+SEND_ADDRESS = Byte.pack(3)
+SET_BAUD     = Byte.pack(4)
+SET_MODE     = Byte.pack(5)
 
 #/*-----------DAC--------*/
-DAC =6
-SET_DAC= 1
-SET_CALIBRATED_DAC= 2
+DAC                = Byte.pack(6)
+SET_DAC            = Byte.pack(1)
+SET_CALIBRATED_DAC = Byte.pack(2)
 
 
 #/*--------WAVEGEN-----*/
-WAVEGEN =7
-SET_WG= 1
-SET_SQR1 = 3
-SET_SQR2 = 4
-SET_SQRS = 5
-TUNE_SINE_OSCILLATOR = 6
-SQR4	=7
-MAP_REFERENCE = 8
-SET_BOTH_WG = 9
-SET_WAVEFORM_TYPE = 10
-SELECT_FREQ_REGISTER = 11
-DELAY_GENERATOR = 12
-SET_SINE1 = 13
-SET_SINE2 = 14
+WAVEGEN              = Byte.pack(7)
+SET_WG               = Byte.pack(1)
+SET_SQR1             = Byte.pack(3)
+SET_SQR2             = Byte.pack(4)
+SET_SQRS             = Byte.pack(5)
+TUNE_SINE_OSCILLATOR = Byte.pack(6)
+SQR4	             = Byte.pack(7)
+MAP_REFERENCE        = Byte.pack(8)
+SET_BOTH_WG          = Byte.pack(9)
+SET_WAVEFORM_TYPE    = Byte.pack(10)
+SELECT_FREQ_REGISTER = Byte.pack(11)
+DELAY_GENERATOR      = Byte.pack(12)
 
-LOAD_WAVEFORM1 = 15
-LOAD_WAVEFORM2 = 16
+LOAD_WAVEFORM1 = Byte.pack(15)
+LOAD_WAVEFORM2 = Byte.pack(16)
+
 #/*-----digital outputs----*/
-DOUT= 8
-SET_STATE = 1
+DOUT      = Byte.pack(8)
+SET_STATE = Byte.pack(1)
 
 #/*-----digital inputs-----*/
-DIN  = 9
-GET_STATE = 1
-GET_STATES = 2
+DIN        = Byte.pack(9)
+GET_STATE  = Byte.pack(1)
+GET_STATES = Byte.pack(2)
 
 
-ID1	 = 0
-ID2  = 1
-ID3  = 2
-ID4  = 3
-LMETER = 4
+ID1	   = Byte.pack(0)
+ID2        = Byte.pack(1)
+ID3        = Byte.pack(2)
+ID4        = Byte.pack(3)
+LMETER     = Byte.pack(4)
 
 
 #/*------TIMING FUNCTIONS-----*/
-TIMING =10
-GET_TIMING = 1
-GET_PULSE_TIME=2
-GET_DUTY_CYCLE=3
-START_ONE_CHAN_LA =4
-START_TWO_CHAN_LA =5
-START_FOUR_CHAN_LA =6
-FETCH_DMA_DATA =7
-FETCH_INT_DMA_DATA =8
-FETCH_LONG_DMA_DATA =9
-GET_LA_PROGRESS =10
-GET_INITIAL_DIGITAL_STATES = 11
+TIMING                      = Byte.pack(10)
+GET_TIMING                  = Byte.pack(1)
+GET_PULSE_TIME              = Byte.pack(2)
+GET_DUTY_CYCLE              = Byte.pack(3)
+START_ONE_CHAN_LA           = Byte.pack(4)
+START_TWO_CHAN_LA           = Byte.pack(5)
+START_FOUR_CHAN_LA          = Byte.pack(6)
+FETCH_DMA_DATA              = Byte.pack(7)
+FETCH_INT_DMA_DATA          = Byte.pack(8)
+FETCH_LONG_DMA_DATA         = Byte.pack(9)
+GET_LA_PROGRESS             = Byte.pack(10)
+GET_INITIAL_DIGITAL_STATES  = Byte.pack(11)
 
-TIMING_MEASUREMENTS=12
-INTERVAL_MEASUREMENTS=13
-CONFIGURE_COMPARATOR =14
-START_ALTERNATE_ONE_CHAN_LA =15
-START_THREE_CHAN_LA =16
+TIMING_MEASUREMENTS         = Byte.pack(12)
+INTERVAL_MEASUREMENTS       = Byte.pack(13)
+CONFIGURE_COMPARATOR        = Byte.pack(14)
+START_ALTERNATE_ONE_CHAN_LA = Byte.pack(15)
+START_THREE_CHAN_LA         = Byte.pack(16)
 
 #/*--------MISCELLANEOUS------*/
-COMMON =11
+COMMON                = Byte.pack(11)
 
-GET_CTMU_VOLTAGE =1
-GET_CAPACITANCE= 2
-GET_FREQUENCY   =3
-GET_INDUCTANCE  =4
+GET_CTMU_VOLTAGE      = Byte.pack(1)
+GET_CAPACITANCE       = Byte.pack(2)
+GET_FREQUENCY         = Byte.pack(3)
+GET_INDUCTANCE        = Byte.pack(4)
 
-GET_VERSION = 5
+GET_VERSION           = Byte.pack(5)
 
-RETRIEVE_BUFFER     =8
-GET_HIGH_FREQUENCY   =9
-CLEAR_BUFFER		=10
-SET_RGB				=11
-READ_PROGRAM_ADDRESS		=12
-WRITE_PROGRAM_ADDRESS		=13
-READ_DATA_ADDRESS			=14
-WRITE_DATA_ADDRESS			=15
+RETRIEVE_BUFFER       = Byte.pack(8)
+GET_HIGH_FREQUENCY    = Byte.pack(9)
+CLEAR_BUFFER          = Byte.pack(10)
+SET_RGB		      = Byte.pack(11)
+READ_PROGRAM_ADDRESS  = Byte.pack(12)
+WRITE_PROGRAM_ADDRESS = Byte.pack(13)
+READ_DATA_ADDRESS     = Byte.pack(14)
+WRITE_DATA_ADDRESS    = Byte.pack(15)
 
-GET_CAP_RANGE		=16
-SET_ONBOARD_RGB		=17
-READ_LOG = 18
-RESTORE_STANDALONE = 19
+GET_CAP_RANGE	      = Byte.pack(16)
+SET_ONBOARD_RGB	      = Byte.pack(17)
+READ_LOG              = Byte.pack(18)
+RESTORE_STANDALONE    = Byte.pack(19)
+
 #/*---------- BAUDRATE for main comm channel----*/
-SETBAUD				=12
-BAUD9600			=1
-BAUD14400			=2
-BAUD19200			=3
-BAUD28800			=4
-BAUD38400			=5
-BAUD57600			=6
-BAUD115200			=7
-BAUD230400			=8
-BAUD1000000			=9
-#/*-----------NRFL01 radio module----------*/
-NRFL01  =13
-NRF_SETUP =1
-NRF_RXMODE =2
-NRF_TXMODE =3
-NRF_POWER_DOWN =4
-NRF_RXCHAR =5
-NRF_TXCHAR =6
-NRF_HASDATA =7
-NRF_FLUSH =8
-NRF_WRITEREG =9
-NRF_READREG =10
-NRF_GETSTATUS =11
-NRF_WRITECOMMAND =12
-NRF_WRITEPAYLOAD =13
-NRF_READPAYLOAD =14
-NRF_WRITEADDRESS=15
-NRF_TRANSACTION = 16
-NRF_START_TOKEN_MANAGER =17
-NRF_STOP_TOKEN_MANAGER =18
-NRF_TOTAL_TOKENS =19
-NRF_REPORTS = 20
-NRF_WRITE_REPORT =21
-NRF_DELETE_REPORT_ROW =22
+SETBAUD     = Byte.pack(12)
+BAUD9600    = Byte.pack(1)
+BAUD14400   = Byte.pack(2)
+BAUD19200   = Byte.pack(3)
+BAUD28800   = Byte.pack(4)
+BAUD38400   = Byte.pack(5)
+BAUD57600   = Byte.pack(6)
+BAUD115200  = Byte.pack(7)
+BAUD230400  = Byte.pack(8)
+BAUD1000000 = Byte.pack(9)
 
-NRF_WRITEADDRESSES=23
+#/*-----------NRFL01 radio module----------*/
+NRFL01                  = Byte.pack(13)
+NRF_SETUP               = Byte.pack(1)
+NRF_RXMODE              = Byte.pack(2)
+NRF_TXMODE              = Byte.pack(3)
+NRF_POWER_DOWN          = Byte.pack(4)
+NRF_RXCHAR              = Byte.pack(5)
+NRF_TXCHAR              = Byte.pack(6)
+NRF_HASDATA             = Byte.pack(7)
+NRF_FLUSH               = Byte.pack(8)
+NRF_WRITEREG            = Byte.pack(9)
+NRF_READREG             = Byte.pack(10)
+NRF_GETSTATUS           = Byte.pack(11)
+NRF_WRITECOMMAND        = Byte.pack(12)
+NRF_WRITEPAYLOAD        = Byte.pack(13)
+NRF_READPAYLOAD         = Byte.pack(14)
+NRF_WRITEADDRESS        = Byte.pack(15)
+NRF_TRANSACTION         = Byte.pack(16)
+NRF_START_TOKEN_MANAGER = Byte.pack(17)
+NRF_STOP_TOKEN_MANAGER  = Byte.pack(18)
+NRF_TOTAL_TOKENS        = Byte.pack(19)
+NRF_REPORTS             = Byte.pack(20)
+NRF_WRITE_REPORT        = Byte.pack(21)
+NRF_DELETE_REPORT_ROW   = Byte.pack(22)
+
+NRF_WRITEADDRESSES      = Byte.pack(23)
 
 #---------Non standard IO protocols--------
 
-NONSTANDARD_IO = 14
-HX711_HEADER = 1
-HCSR04_HEADER = 2
-AM2302_HEADER = 3
-TCD1304_HEADER = 4
-STEPPER_MOTOR = 5
+NONSTANDARD_IO = Byte.pack(14)
+HX711_HEADER   = Byte.pack(1)
+HCSR04_HEADER  = Byte.pack(2)
+AM2302_HEADER  = Byte.pack(3)
+TCD1304_HEADER = Byte.pack(4)
+STEPPER_MOTOR  = Byte.pack(5)
+
 #--------COMMUNICATION PASSTHROUGHS--------
 #Data sent to the device is directly routed to output ports such as (SCL, SDA for UART)
 
-PASSTHROUGHS = 15
-PASS_UART = 1
+PASSTHROUGHS = Byte.pack(15)
+PASS_UART    = Byte.pack(1)
 
 #/*--------STOP STREAMING------*/
-STOP_STREAMING =253
+STOP_STREAMING = Byte.pack(253)
 
 #/*------INPUT CAPTURE---------*/
 #capture modes
-EVERY_SIXTEENTH_RISING_EDGE = 0b101
-EVERY_FOURTH_RISING_EDGE    = 0b100
-EVERY_RISING_EDGE           = 0b011
-EVERY_FALLING_EDGE          = 0b010
-EVERY_EDGE                  = 0b001
-DISABLED		    = 0b000
+EVERY_SIXTEENTH_RISING_EDGE = Byte.pack(0b101)
+EVERY_FOURTH_RISING_EDGE    = Byte.pack(0b100)
+EVERY_RISING_EDGE           = Byte.pack(0b011)
+EVERY_FALLING_EDGE          = Byte.pack(0b010)
+EVERY_EDGE                  = Byte.pack(0b001)
+DISABLED		    = Byte.pack(0b000)
+
 #/*--------Chip selects-----------*/
-CSA1	=1
-CSA2	=2
-CSA3	=3
-CSA4	=4
-CSA5	=5
-CS1		=6
-CS2		=7
+CSA1 = Byte.pack(1)
+CSA2 = Byte.pack(2)
+CSA3 = Byte.pack(3)
+CSA4 = Byte.pack(4)
+CSA5 = Byte.pack(5)
+CS1  = Byte.pack(6)
+CS2  = Byte.pack(7)
 
 #resolutions
-TEN_BIT=10
-TWELVE_BIT=12
+TEN_BIT    = Byte.pack(10)
+TWELVE_BIT = Byte.pack(12)
 
 class Singleton(type):
     _instances = {}
@@ -230,13 +237,13 @@ class Singleton(type):
         return cls._instances[cls]
 
 def InttoString(val):
-	return	chr(val&0xff) +  chr((val>>8)&0xff)
+	return	ShortInt.pack(int(val))
 
 def StringtoInt(string):
-	return	ord(string[0]) |  (ord(string[1])<<8)
+	return	ShortInt.unpack(string)[0]
 
 def StringtoLong(string):
-	return	ord(string[0]) |  (ord(string[1])<<8) |  (ord(string[2])<<16) |  (ord(string[3])<<24)
+	return	Integer.unpack(string)[0]
 
 def getval12(val):
 	return val*3.3/4095
@@ -260,6 +267,3 @@ def getLx(f1,f2,f3,Ccal):
 
 def reverse_bits(x):
 	return int('{:08b}'.format(x)[::-1], 2)
-
-
-
